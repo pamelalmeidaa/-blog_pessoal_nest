d4ac31d7ab7f56ff0cf4adf6c61c4a63f870752d
Pamela <contatopamela@outlook.com>
2024-08-06 20:04:34 -0300

    Deploy - blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,25 @@
-import {PostagemModule} from './postagem/postagem.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
 import { ProdService } from './data/service/prod.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
-        imports: [ConfigModule],
-    }),,
-    PostagemModule,
-    TemaModule,
+      useClass: ProdService, 
+      imports: [ConfigModule], 
+    }),
+    PostagemModule, 
+    TemaModule, 
     AuthModule,
-    UsuarioModule,
+    UsuarioModule, 
   ],
-  controllers: [AppController],
-  providers: [],
+  controllers: [AppController], 
+  providers: [], 
 })
 export class AppModule {}
