ab497fe402d8034b3271a6e377b7b6417d60b82d
Pamela <contatopamela@outlook.com>
2024-08-06 19:25:31 -0300

    Deploy do Projeto Blog Pessoal

--- src/tema/services/tema.service.ts
+++ src/tema/services/tema.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeleteResult, ILike, Repository } from "typeorm";
-import { Tema } from "../entities/tema.entity";
+import { Tema } from "../entities/Tema.entity";
 
 @Injectable()
 export class TemaService {
--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { TemaController } from "./controllers/tema.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TemaService } from "./services/tema.service";
-import { Tema } from "./entities/tema.entity";
+import { Tema } from "./entities/Tema.entity";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Tema])],
